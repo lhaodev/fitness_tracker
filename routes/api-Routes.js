137433d1c1055c1263565b621820f35c2474fb66
Lu Hao <haolu1125@gmail.com>
2021-03-14 22:19:04 -0400

    api-routes

--- routes/api-Routes.js
+++ routes/api-Routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
