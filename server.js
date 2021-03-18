610d87741db6bcf1fc9fed882263ee6ee6b79b3c
Lu Hao <haolu1125@gmail.com>
2021-03-17 23:11:37 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -15,7 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+
+
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+
+
+
+).then(() => {
     console.log("connected to database")
 });
 
