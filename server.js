bec18d13cf79ed3ded84b3269cba3836ed23dd1a
Lu Hao <haolu1125@gmail.com>
2021-03-18 18:04:07 -0400

    heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
 
     //connect mangodb -
-    //test again
+    //test again -
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
